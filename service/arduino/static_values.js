aa596c8d118d14e570120b52f322892a8f4436fc
Javier Cabrera <cabreration>
2020-08-10 17:57:18 -0600

    web service added

---
+++ service/arduino/static_values.js
@@ -0,0 +1,7 @@
+Common = {
+    WEIGHT: 0,
+    EXISTS: false,
+    SANITIZER: 0
+}
+
+module.exports = Common
---
+++ service/mobile/mail.js
@@ -0,0 +1,9 @@
+let Common = require('../arduino/static_values')
+
+module.exports = (app) => {
+
+    app.get('/mail', async(req, res) => {
+        res.send({ peso: Common.WEIGHT, objeto: Common.EXISTS, liquido: Common.SANITIZER })
+    })
+
+}
