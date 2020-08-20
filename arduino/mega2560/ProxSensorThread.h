4ed1a041ef9c04b330a3daf56bfc950d7c6818b8
Leo Aguilar <leoaguilarsanchez97@gmail.com>
2020-08-19 21:51:29 -0600

    Formato al codigo de mega2560 y arreglado el folder donde se encontraba

---
+++ arduino/mega2560/ProxSensorThread.h
@@ -0,0 +1,22 @@
+
+#include "Thread.h"
+#include "ThreadController.h"
+#include <TimerOne.h>
+
+class ProxSensorThread: public Thread {
+  private:
+    byte pin;
+
+  public:
+    byte value;
+    
+    ProxSensorThread(byte _pin): pin {_pin}, Thread()
+    {
+      pinMode(pin, INPUT);
+    }
+
+    void run() {
+      value = !digitalRead(pin);
+      runned();
+    }
+};
