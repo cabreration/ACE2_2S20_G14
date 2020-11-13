670ae9f1c3c411362fce15ee54a914f80e02a612
Edgar Aldana <arnoldo201@hotmail.com>
2020-11-12 22:37:38 -0600

    .

--- proyecto 2/api2/app/routes/persona.js
+++ proyecto 2/api2/app/routes/persona.js
@@ -9,30 +9,11 @@ module.exports = app => {
     // Agrega un usuario con temperatura a la base de datos
     router.get("/val", persona.UserTemperatura); // <-- DESCOMENTAR
 
-    /*router.get("/val", (req, res) => {
-        let temp = parseInt(req.query.temp) / 1000;
-        let time = parseInt(req.query.tiem) / 1000;
-
-        console.log(`Temperatura: ${temp} °C`);
-        console.log(`Tiempo: ${time} segundos`)
-
-        res.send("agregado");
-    });*/
-
     // Agrega un usuario con temperatura a la base de datos
     router.get("/obtenerPersonas", persona.getAll);
 
     // Agrega un usuario con temperatura a la base de datos
     router.get("/empezo", persona.empezo);  // <-- DESCOMENTAR
-
-    //SOLO PARA PRUEBAS, RETORNAR SI 1/10 de las veces que se pida.
-    /*router.get("/empezo", (req, res) =>{
-        if (Math.floor(Math.random() * 10) % 10){
-            return res.send("no");
-        }
-
-        return res.send("si");
-    })*/
 
     // Devuelve el tiempo promedio
     router.get("/getTiempoPromedio", persona.getTiempoPromedio);
