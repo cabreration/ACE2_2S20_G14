232c91ece6fa92f38516fbdc8fb1848a574a550c
Javier A. Cabrera <cabreraction@gmail.com>
2020-11-13 23:37:40 -0600

    dashboard added

--- proyecto 2/Fldsmdfr/src/app/app.module.ts
+++ proyecto 2/Fldsmdfr/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { APIDataService } from './apidata.service';
 import { RecordsComponent } from './records/records.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartsModule } from 'ng2-charts';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ChartsModule
   ],
   providers: [ APIDataService ],
   bootstrap: [AppComponent]
--- proyecto 2/Fldsmdfr/src/app/dashboard/dashboard.component.ts
+++ proyecto 2/Fldsmdfr/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { APIDataService } from '../apidata.service';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,16 +10,78 @@ import { APIDataService } from '../apidata.service';
 })
 export class DashboardComponent implements OnInit {
 
+  avgTime = null;
+  promedioHoras = null;
+  promedioDias = null;
+
+  // ----------------------------- First One
+  public firstChartData = [
+    { data: [], label: 'Promedio Visitantes por Hora' },
+  ];
+  public firstChartLabels = null;
+  public firstChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  }
+  public ChartColors: Color[] = [
+    {
+      borderColor: 'black',
+      backgroundColor: 'rgba(255,0,0,0.3)',
+    },
+  ];
+  public firstChartLegend = true;
+  public firstChartType = 'bar';
+  // -----------------------------
+
+  // ----------------------------- First One
+  public secondChartData = [
+    { data: [], label: 'Promedio Visitantes por Dia de la Semana' },
+  ];
+  public secondChartLabels = null;
+  public secondChartOptions = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  }
+  public secondChartLegend = true;
+  public secondChartType = 'bar';
+  // -----------------------------
+
   constructor(private api: APIDataService) { }
 
   ngOnInit(): void {
-    let observer = this.api.GetRequest('/getTiempoPromedio').subscribe(res => console.log(res))
+    let observer = this.api.GetRequest('/getTiempoPromedio').subscribe(res => this.avgTime = res.tiempoPromedio.toFixed(2))
 
-    let observer2 = this.api.GetRequest('/getConcurrenciaHoras').subscribe(res => console.log(res))
+    //let observer2 = this.api.GetRequest('/getConcurrenciaHoras').subscribe(res => console.log(res))
 
-    let observer3 = this.api.GetRequest('/getPromedioHoras').subscribe(res => console.log(res))
+    let observer3 = this.api.GetRequest('/getPromedioHoras').subscribe(res => {
+      console.log(res);
+      this.promedioHoras = res.horas;
+      this.prepareFirst();
+    })
 
-    let observer4 = this.api.GetRequest('/getConcurrenciaDias').subscribe(res => console.log(res))
+    let observer4 = this.api.GetRequest('/getConcurrenciaDias').subscribe(res => {
+      console.log(res);
+      this.promedioDias = res.dias;
+      this.prepareSecond();
+    })
+  }
+
+  prepareFirst() {
+    this.firstChartLabels = []
+    this.firstChartData[0].data = []
+    this.promedioHoras.forEach(entry => {
+      this.firstChartLabels.push(entry.hora);
+      this.firstChartData[0].data.push(entry.personas.toFixed(2))
+    });
+  }
+
+  prepareSecond() {
+    this.secondChartLabels = []
+    this.secondChartData[0].data = []
+    this.promedioDias.forEach(entry => {
+      this.secondChartLabels.push(entry.dia);
+      this.secondChartData[0].data.push(entry.personas.toFixed(2))
+    });
   }
 
 }
